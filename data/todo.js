e1051222291b864b07a7fdfe516d1f64aecdebb2
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-08-18 14:44:09 -0700

    adds todo js and properly loads seed data

---
+++ data/todo.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    task: 'walk the dog',
+    completed: false
+  },
+  {
+    task: 'clean out the garage',
+    completed: false
+  },
+  {
+    task: 'go grocery shopping for the week',
+    completed: false
+  }
+];
