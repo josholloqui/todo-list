6b7b042b7a99a4c97e671f313dc92f869fb3bdc4
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-08-18 15:41:12 -0700

    adds actual proper seeding of data

--- data/todo.js
+++ data/todo.js
@@ -1,14 +1,17 @@
 module.exports = [
   {
     task: 'walk the dog',
-    completed: false
+    completed: false,
+    owner_id: 1
   },
   {
     task: 'clean out the garage',
-    completed: false
+    completed: false,
+    owner_id: 2
   },
   {
     task: 'go grocery shopping for the week',
-    completed: false
+    completed: false,
+    owner_id: 3
   }
 ];
